3ba466226e4d1d82040dc200eb712975d8df40c6
jaebong <yuwoqhd@gmail.com>
2021-04-11 21:50:29 +0900

    Add: CashTag Regex

--- lib/detector/sample_regular_expressions.dart
+++ lib/detector/sample_regular_expressions.dart
@@ -8,24 +8,10 @@ const _numbers = '0-9０-９';
 
 const _englishLetters = 'a-zA-Zａ-ｚＡ-Ｚ';
 
-const _japaneseLetters = 'ぁ-んァ-ン一-龠';
-
 const _koreanLetters = '\u1100-\u11FF\uAC00-\uD7A3';
 
-const _spanishLetters = 'áàãâéêíóôõúüçÁÀÃÂÉÊÍÓÔÕÚÜÇ';
-
-const _arabicLetters = '\u0621-\u064A';
-
-const _thaiLetters = '\u0E00-\u0E7F';
-
-const detectionContentLetters = _symbols +
-    _numbers +
-    _englishLetters +
-    _japaneseLetters +
-    _koreanLetters +
-    _spanishLetters +
-    _arabicLetters +
-    _thaiLetters;
+const detectionContentLetters =
+    _symbols + _numbers + _englishLetters + _koreanLetters;
 
 const urlRegexContent = "((http|https)://)(www.)?" +
     "[a-zA-Z0-9@:%._\\+~#?&//=]" +
@@ -46,12 +32,27 @@ final atSignRegExp = RegExp(
   multiLine: true,
 );
 
+final cashTagRegExp = RegExp(
+  "(?!\\n)(?:^|\\s)([\$]([$detectionContentLetters]+))",
+  multiLine: true,
+);
+
 final urlRegex = RegExp(
   urlRegexContent,
   multiLine: true,
 );
 
 /// Regular expression when you select decorateAtSign
+final cashTaghashTagAtSignUrlRegExp = RegExp(
+  "(?!\\n)(?:^|\\s)([#@\$]([$detectionContentLetters]+))|$urlRegexContent",
+  multiLine: true,
+);
+
+final cashTagAtSignUrlRegExp = RegExp(
+  "(?!\\n)(?:^|\\s)([@\$]([$detectionContentLetters]+))|$urlRegexContent",
+  multiLine: true,
+);
+
 final hashTagAtSignRegExp = RegExp(
   "(?!\\n)(?:^|\\s)([#@]([$detectionContentLetters]+))",
   multiLine: true,
